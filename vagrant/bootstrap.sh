87851b3a13d0c932e280a4bca4d6f3a30a846ecb
Kevin Wang <kevixw@gmail.com>
2016-08-29 12:04:51 -0700

    Change vagrant config

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,23 +1,15 @@
 #!/usr/bin/env bash
 
+REPO_URL='https://github.com/kevinxw/opendaylight-setup.git'
+
 apt-get -qq update
 echo Y | apt-get -qq install git
-echo Y | apt-get -qq install zip 
-echo Y | apt-get -qq install openconnect
-echo Y | apt-get -qq install default-jre
-echo Y | apt-get -qq install mininet
-echo Y | apt-get -qq install python-pip
-echo pip install pyang
-echo "cloning scripts"
-git clone -q https://github.com/CiscoDevNet/opendaylight-setup.git ODL
+git clone -q ${REPO_URL} ODL
 cd ODL
-mkdir images
-cd images
-echo "downloading ODL distro"
-wget -q https://nexus.opendaylight.org/content/repositories/opendaylight.release/org/opendaylight/integration/distribution-karaf/0.4.3-Beryllium-SR3/distribution-karaf-0.4.3-Beryllium-SR3.tar.gz
-cd ..
-echo "unpacking ODL distro"
-./unpack-odl 
+./bin/setup-env
+./bin/download_distro release
+./bin/unpack-odl 
 cd ..
 chown -R vagrant:vagrant *
 echo "ready!"
+echo "please change parameters to reflect your setting ups"
