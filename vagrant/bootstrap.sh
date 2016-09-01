4a8a6db901a4588e8ed3414489f5e664f3dfefa7
Kevin Wang <kwang@brocade.com>
2016-09-01 14:16:55 -0700

    Change vagrant config

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -2,14 +2,12 @@
 
 REPO_URL='https://github.com/kevinxw/opendaylight-setup.git'
 
-sudo apt-get update
-sudo apt-get install -y vim git
+sudo apt-get install -y git
 git clone -q ${REPO_URL} ODL
 cd ODL
-./bin/setup-env
-./bin/download_distro release
-./bin/unpack-odl 
+./bin/setup-odl
 cd ..
+sudo apt-get install -y vim
 chown -R vagrant:vagrant *
 echo "ready!"
 echo "please change parameters to reflect your setting ups"
