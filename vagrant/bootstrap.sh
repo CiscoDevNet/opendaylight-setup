51b5d04c21eb663ddbdcb4b13bdc134a637e5a19
Kevin Wang <kevixw@gmail.com>
2016-09-19 01:31:50 -0700

    Update setup-env to include wireshark

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -5,10 +5,9 @@ REPO_URL='https://github.com/kevinxw/opendaylight-setup.git'
 sudo apt-get install -y git
 git clone -q ${REPO_URL} ODL
 cd ODL
-./bin/setup-odl
+./bin/setup-env
+./bin/setup-odl beryllium
 cd ..
-sudo apt-get install -y vim
-#sudo gpasswd -a vagrant wireshark
 chown -R vagrant:vagrant *
 # now create a shortcut to Desktop
 ln -s $HOME/ODL $HOME/Desktop/ODL
